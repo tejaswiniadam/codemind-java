bbcf86f05f41c6f44a625575751d1a365d154505
Tejaswini Adam <155874731+tejaswiniadam@users.noreply.github.com>
2024-01-25 21:43:45 +0530

    import java.util.Scanner; public class Avgwt{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int avg=sc.nextInt();         int b1=sc.nextInt();         int b2=sc.nextInt();         System.out.println(+(3*avg)-b1-b2);     } }

---
+++ Average_weight.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Avgwt{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int avg=sc.nextInt();
+        int b1=sc.nextInt();
+        int b2=sc.nextInt();
+        System.out.println(+(3*avg)-b1-b2);
+    }
+}
