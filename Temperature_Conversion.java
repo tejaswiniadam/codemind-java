a075547205d333b7016ebdca7fca33aafac2aa50
Tejaswini Adam <155874731+tejaswiniadam@users.noreply.github.com>
2024-01-31 11:56:35 +0530

    import java.util.Scanner; public class Temp{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         double c=sc.nextDouble();         double f=32+(c*9/5);         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Temp{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double c=sc.nextDouble();
+        double f=32+(c*9/5);
+        System.out.printf("%.2f",f);
+    }
+}
