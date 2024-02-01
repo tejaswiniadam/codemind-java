b80f92aa5aff5012d93252d7f6941c8adb129678
Tejaswini Adam <155874731+tejaswiniadam@users.noreply.github.com>
2024-02-01 21:59:25 +0530

    import java.util.Scanner; public class EvenOdd{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%2==0)             System.out.println("Even");         else             System.out.println("Odd");     } }

--- Check_if_number_is_even_or_odd.java
+++ Check_if_number_is_even_or_odd.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class Evenodd{
+public class EvenOdd{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        if(x%2==0)
+        int n=sc.nextInt();
+        if(n%2==0)
             System.out.println("Even");
         else
             System.out.println("Odd");
