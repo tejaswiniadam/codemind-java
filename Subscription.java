1d10acc0d45f3c502a7b592103a090e2ef91e589
Tejaswini Adam <155874731+tejaswiniadam@users.noreply.github.com>
2024-02-01 11:47:22 +0530

    import java.util.Scanner; public class Subscription{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x>40)             System.out.println("YES");         else             System.out.println("NO");     } }

---
+++ Subscription.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Subscription{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x>40)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+}
