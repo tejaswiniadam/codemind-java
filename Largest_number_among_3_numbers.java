068223010f8665764ce9949797e28654c138a2eb
Tejaswini Adam <155874731+tejaswiniadam@users.noreply.github.com>
2024-02-01 21:44:59 +0530

    import java.util.Scanner; public class Largest{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         if(x>y && x>z)             System.out.println(x);         else if(y>z)             System.out.println(y);         else             System.out.println(z);     } }

---
+++ Largest_number_among_3_numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Largest{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        if(x>y && x>z)
+            System.out.println(x);
+        else if(y>z)
+            System.out.println(y);
+        else
+            System.out.println(z);
+    }
+}
