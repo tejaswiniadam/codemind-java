e5a5e2706283ffe84db0cc9fb59b79624ae75a96
Tejaswini Adam <155874731+tejaswiniadam@users.noreply.github.com>
2024-03-08 19:21:46 +0530

    import java.util.Scanner;
    public class Multiplication {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
            int a = sc.nextInt();
            int b = sc.nextInt();
            for (int i = a; i <= b; i++) {
                System.out.println(n + " x " + i + " = " + (n * i));
            }
        }
    }

---
+++ Multiplication_Table_-_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = a; i <= b; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
